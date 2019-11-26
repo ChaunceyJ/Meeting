e109006741821558a54fa187db54359c3408df69
DESKTOP-VBV4S1E\97409 <974093989@qq.com>
2019-11-26 19:54:42 +0800

    Calendar以及UserCalendar model

---
+++ src/main/java/com/tongji/meeting/model/UserCalendar.java
@@ -0,0 +1,40 @@
+package com.tongji.meeting.model;
+
+public class UserCalendar {
+    private int calendarId;
+    private int userId;
+    private boolean disturb;
+    private String role;
+
+    public int getCalendarId() {
+        return calendarId;
+    }
+
+    public void setCalendarId(int calendarId) {
+        this.calendarId = calendarId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isDisturb() {
+        return disturb;
+    }
+
+    public void setDisturb(boolean disturb) {
+        this.disturb = disturb;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
